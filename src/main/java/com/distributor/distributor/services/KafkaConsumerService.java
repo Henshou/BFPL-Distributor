c81946991d1266caf658b4da60018efeb30769c3
Henshou <gerrardgrs@gmail.com>
2026-05-01 19:36:00 +0700

    feat: telemetry event for metrics extraction

--- src/main/java/com/distributor/distributor/services/KafkaConsumerService.java
+++ src/main/java/com/distributor/distributor/services/KafkaConsumerService.java
@@ -1,7 +1,10 @@
 package com.distributor.distributor.services;
 
 import com.distributor.distributor.model.ParsedLog;
+
+import com.distributor.distributor.model.TelemetryEvent;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 import tools.jackson.databind.ObjectMapper;
 
@@ -9,19 +12,74 @@ import tools.jackson.databind.ObjectMapper;
 public class KafkaConsumerService {
     private final KafkaPublisherService kafkaPublisherService;
     private final ObjectMapper objectMapper;
+    private final TelemetryPublisherService telemetryPublisherService;
 
-    public KafkaConsumerService(KafkaPublisherService kafkaPublisherService, ObjectMapper objectMapper) {
+    public KafkaConsumerService(KafkaPublisherService kafkaPublisherService, ObjectMapper objectMapper, TelemetryPublisherService telemetryPublisherService) {
         this.kafkaPublisherService = kafkaPublisherService;
         this.objectMapper = objectMapper;
+        this.telemetryPublisherService = telemetryPublisherService;
     }
 
     @KafkaListener(topics = "processor-topic", groupId = "distributor-group")
-    public void consumeParsedLog(String message) {
+    public void consumeParsedLog(String message,
+                                 @Header("processor_publish_time") byte[] processorPublishBytes,
+                                 @Header("collector_start_time") byte[] collectorStartBytes,
+                                 @Header("source_file_name") byte[] fileBytes,
+                                 @Header("record_id") byte[] recordIdBytes,
+                                 @Header("batch_id") byte[] batchIdBytes) {
         try{
+            long publishTime = Long.parseLong(new String(processorPublishBytes));
+            long collectorStartTime = Long.parseLong(new String(collectorStartBytes));
+            long receiveTime = System.currentTimeMillis();
+            String fileName = new String(fileBytes);
+            String recordId = new String(recordIdBytes);
+            String batchId = new String(batchIdBytes);
+
+            TelemetryEvent messagingDelayEvent = new TelemetryEvent(
+                    recordId,
+                    batchId,
+                    fileName,
+                    "distributor",
+                    "messaging.delay.processor.distributor",
+                    receiveTime - publishTime,
+                    System.currentTimeMillis()
+            );
+
+            telemetryPublisherService.publishTelemetry(messagingDelayEvent);
+
+            long processingStartTime = System.currentTimeMillis();
+
             ParsedLog log = objectMapper.readValue(message, ParsedLog.class);
+
             System.out.println("Received parsed log: " + log);
 
             kafkaPublisherService.publishParsedLog(log);
+
+            long processingEndTime = System.currentTimeMillis();
+
+            TelemetryEvent processingTimeEvent = new TelemetryEvent(
+                    recordId,
+                    batchId,
+                    fileName,
+                    "distributor",
+                    "distributor.record.process.time",
+                    processingEndTime - processingStartTime,
+                    System.currentTimeMillis()
+            );
+
+            telemetryPublisherService.publishTelemetry(processingTimeEvent);
+
+            TelemetryEvent dataLatencyEvent = new TelemetryEvent(
+                    recordId,
+                    batchId,
+                    fileName,
+                    "distributor",
+                    "data.latency.e2e",
+                    processingEndTime - collectorStartTime,
+                    System.currentTimeMillis()
+            );
+
+            telemetryPublisherService.publishTelemetry(dataLatencyEvent);
 
         } catch (Exception e) {
             e.printStackTrace();
---
+++ src/main/java/com/distributor/distributor/services/TelemetryPublisherService.java
@@ -0,0 +1,26 @@
+package com.distributor.distributor.services;
+
+import com.distributor.distributor.model.TelemetryEvent;
+import tools.jackson.databind.ObjectMapper;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TelemetryPublisherService {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    public TelemetryPublisherService(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public void publishTelemetry(TelemetryEvent telemetryEvent) {
+        try {
+            String message = objectMapper.writeValueAsString(telemetryEvent);
+            kafkaTemplate.send("dq-metrics-topic", message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
